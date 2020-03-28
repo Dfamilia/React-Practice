841bf3be7d9f296f5f7cdcb8398c8b2823ee091b
dfamilia <dfamilia@intellisys.com.do>
2020-03-28 17:01:57 -0400

    define states on navbar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 
 export default class Popular extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: "All"
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+
   render() {
     const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
     return (
       <div>
         <ul className="flex-center">
           {languages.map(language => (
-            <li key={languages}>
-              <button className="btn-clear nav-link">{language}</button>
+            <li key={language}>
+              <button
+                style={
+                  language === this.state.selectedLanguage
+                    ? { color: "rgb(187, 46, 31)" }
+                    : null
+                }
+                onClick={() => this.updateLanguage(language)}
+                className="btn-clear nav-link"
+              >
+                {language}
+              </button>
             </li>
           ))}
         </ul>
