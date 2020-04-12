193bb394d31f4778cfcd2110d61e27f99afc9c5e
dfamilia <dfamilia@intellisys.com.do>
2020-04-11 22:42:09 -0400

    add player functionality

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { FaUserFriends, FaFighterJet, FaTrophy } from "react-icons/fa";
+import PropTypes, { checkPropTypes } from "prop-types";
 
 const Instructions = (props) => {
   return (
@@ -27,11 +28,104 @@ const Instructions = (props) => {
   );
 };
 
-export default class Battle extends Component {
+class PlayerInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.username);
+  }
+
   render() {
+    return (
+      <form className="column-player" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="player-label">
+          {this.props.label}
+        </label>
+
+        <div className="row player-inputs">
+          <input
+            type="text"
+            id="username"
+            placeholder="github username"
+            autoComplete="off"
+            onChange={this.handleChange}
+            value={this.state.username}
+          />
+          <button
+            className="btn dark-btn"
+            type="submit"
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
+export default class Battle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player,
+    });
+  }
+
+  render() {
+    const { playerOne, playerTwo } = this.state;
     return (
       <Fragment>
         <Instructions />
+        <div className="players-container">
+          <h1 className="center-text header-lg">Players</h1>
+          <div className="row space-around">
+            {playerOne === null && (
+              <PlayerInput
+                label="Player One"
+                onSubmit={(player) => this.handleSubmit("playerOne", player)}
+              />
+            )}
+
+            {playerTwo === null && (
+              <PlayerInput
+                label="Player Two"
+                onSubmit={(player) => this.handleSubmit("playerTwo", player)}
+              />
+            )}
+          </div>
+        </div>
       </Fragment>
     );
   }
