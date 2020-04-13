e9269454ef008aa69e741cd484969974ee3a6c71
dfamilia <dfamilia@intellisys.com.do>
2020-04-13 16:15:26 -0400

    rehusable component

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,5 +1,14 @@
 import React, { Component } from "react";
 import { battle } from "../utils/api";
+import {
+  FaCompass,
+  FaBriefcase,
+  FaUsers,
+  FaUserFriends,
+  FaCode,
+  FaUser,
+} from "react-icons/fa";
+import Card from "./Card";
 
 export default class Results extends Component {
   constructor(props) {
@@ -32,10 +41,87 @@ export default class Results extends Component {
       });
   }
   render() {
+    const { winner, loser, error, loading } = this.state;
+
+    if (loading === true) {
+      return <p>LOADING...</p>;
+    }
+
+    if (error) {
+      return <p className="center-text error">{error}</p>;
+    }
+
     return (
-      <div>
-        Results
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      <div className="grid space-around container-sm">
+        <Card
+          header={winner.score === loser.score ? "Tie" : "Winner"}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ul className="card-list">
+            <li>
+              <FaUser color="rgb(239, 115, 115)" size={22} />
+              {winner.profile.name}
+            </li>
+            {winner.profile.location && (
+              <li>
+                <FaCompass color="rgb(144, 115, 255)" size={22} />
+                {winner.profile.location}
+              </li>
+            )}
+            {winner.profile.company && (
+              <li>
+                <FaBriefcase color="#795548" size={22} />
+                {winner.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color="rgb(129, 195, 245)" size={22} />
+              {winner.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color="rgb(64, 183, 95)" size={22} />
+              {winner.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </Card>
+
+        <Card
+          header={winner.score === loser.score ? "Tie" : "Loser"}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          href={loser.profile.html_url}
+          name={loser.profile.login}
+        >
+          <ul className="card-list">
+            <li>
+              <FaUser color="rgb(239, 115, 115)" size={22} />
+              {loser.profile.name}
+            </li>
+            {loser.profile.location && (
+              <li>
+                <FaCompass color="rgb(144, 115, 255)" size={22} />
+                {loser.profile.location}
+              </li>
+            )}
+            {loser.profile.company && (
+              <li>
+                <FaBriefcase color="#795548" size={22} />
+                {loser.profile.company}
+              </li>
+            )}
+            <li>
+              <FaUsers color="rgb(129, 195, 245)" size={22} />
+              {loser.profile.followers.toLocaleString()} followers
+            </li>
+            <li>
+              <FaUserFriends color="rgb(64, 183, 95)" size={22} />
+              {loser.profile.following.toLocaleString()} following
+            </li>
+          </ul>
+        </Card>
       </div>
     );
   }
